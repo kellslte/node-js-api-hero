6faeb9dcc6f7c93d56c4a27b6afce5f36b691899
Favour Max-Oti <maxotif@gmail.com>
2025-06-13 10:40:01 +0100

    initial commi

---
+++ class-one/index.js
@@ -0,0 +1,88 @@
+console.log("Hello World");
+
+// Basic Concepts
+// 1. Variables
+// 2. Data Types
+// 3. Operators
+// 4. Control Flow
+// 5. Functions
+// 6. Objects
+// 7. Arrays
+// 8. Loops
+// 9. Conditionals
+// 10. Error Handling
+
+// Variables
+// Mutable
+let name = "John";
+let address = '123 Main St';
+let surname = `Doe`;
+// Initialization
+name = 'Chinecherem'; 
+// Declaration
+let school;
+school = 'University of Lagos';
+// Immutable
+const age = 23;
+
+// . Data Types
+// 1. Primitive
+// 2. Non-Primitive
+
+// Primitive
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+
+// Non-Primitive
+// 1. Object
+// 2. Array
+// 3. Function
+
+// Operators
+// 1. Arithmetic Operators
+// 2. Assignment Operators
+// 3. Comparison Operators
+// 4. Logical Operators
+// 5. Bitwise Operators
+
+// Comparison Operators
+// 1. ==
+// 2. ===
+// 3. !=
+// 4. !==
+// 5. >
+// 6. <
+// 7. >=
+// 8. <=
+
+5 == '5' // true
+6 === '6' // false
+
+7 != '7' // false
+8 !== '8' // true
+
+const number = 10;
+
+11 <= number;
+
+// Logical Operators
+// 1. &&
+// 2. ||
+// 3. !
+
+const isStudent = true;
+const isTeacher = false;
+
+isStudent && isTeacher; // false
+isStudent || isTeacher; // true
+!isStudent; // false
+
+// Bitwise Operators
+// 1. &
+// 2. |
+// 3. ^
+// 4. ~
+// 5. <<
